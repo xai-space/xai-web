9111c59aa1007726e126b5b8a49e5e4dc0d1f673
Birch <143273008+birchwork@users.noreply.github.com>
2024-12-20 22:58:30 +0800

    feat: noticelist update

--- src/views/notification/components/notice-card-list.tsx
+++ src/views/notification/components/notice-card-list.tsx
@@ -10,21 +10,15 @@ import {
 } from '@/components/ui/card'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { defaultAgentLogo } from '@/config/link'
-<<<<<<< HEAD
 import { useInfiniteScroll, useRequest } from 'ahooks'
 import { isEmpty, get } from 'lodash-es'
 import { NoticeAtion } from '@/api/user/types'
 import { putReadNotices } from '@/api/user'
 import { useEffect } from 'react'
-=======
-import { useInfiniteScroll } from 'ahooks'
-import { isEmpty, get } from 'lodash-es'
->>>>>>> e7d9784 (feat: notice)
 interface Result {
   list: UserNotificationList[]
   noMore?: boolean
 }
-<<<<<<< HEAD
 interface NoticeCardListProps {
   action: string
 }
@@ -82,31 +76,6 @@ const NoticeCardList = ({ action }: NoticeCardListProps) => {
   return (
     <div className="pt-20">
       {data?.list.map((item, index) => (
-=======
-const NoticeCardList = () => {
-  const getLoadMoreList = async (): Promise<Result> => {
-    const { data } = await aiApi.getNotifications({
-      page: 1,
-      limit: 20,
-    })
-    return {
-      list: data.list,
-      noMore: data.list.length < 20,
-    }
-  }
-
-  const { data, loading, loadMore, loadingMore, noMore } = useInfiniteScroll(
-    getLoadMoreList,
-    {
-      target: document,
-      isNoMore: (d) => d?.noMore === true,
-    }
-  )
-
-  return (
-    <div className="pt-20">
-      {data?.list.map(({ data }, index) => (
->>>>>>> e7d9784 (feat: notice)
         <div className="mb-4" key={index}>
           <Card>
             <CardHeader className="flex flex-row items-center">
@@ -114,13 +83,8 @@ const NoticeCardList = () => {
                 <Avatar>
                   <AvatarImage
                     src={
-<<<<<<< HEAD
                       isEmpty(item.data.from_user.logo)
                         ? `${staticUrl}${item.data.from_user.logo}`
-=======
-                      isEmpty(data.from_user.logo)
-                        ? `${staticUrl}${data.from_user.logo}`
->>>>>>> e7d9784 (feat: notice)
                         : defaultAgentLogo
                     }
                   />
@@ -128,7 +92,6 @@ const NoticeCardList = () => {
                 </Avatar>
               </div>
               <div>
-<<<<<<< HEAD
                 <CardTitle>
                   {get(item, 'data.from_user.name', '-')}
 
@@ -137,12 +100,6 @@ const NoticeCardList = () => {
                 <CardDescription>
                   <div className="line-clamp-3 overflow-hidden text-ellipsis whitespace-pre-wrap">
                     {get(item, 'data.from_user.description', '-')}
-=======
-                <CardTitle>{get(data, 'from_user.name', '-')}</CardTitle>
-                <CardDescription>
-                  <div className="line-clamp-3 overflow-hidden text-ellipsis whitespace-pre-wrap">
-                    {get(data, 'from_user.description', '-')}
->>>>>>> e7d9784 (feat: notice)
                   </div>
                 </CardDescription>
               </div>
