66b16f7ce552d11859504d8a548fff3ffa05a19b
Birch <143273008+birchwork@users.noreply.github.com>
2024-12-21 09:34:27 +0800

    feact:update notice item

--- src/views/notification/index.tsx
+++ src/views/notification/index.tsx
@@ -2,39 +2,37 @@ import { type ReactNode } from 'react'
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs'
 import { PrimaryLayout } from '@/components/layouts/primary'
 import NoticeCardList from './components/notice-card-list'
+import { useTranslation } from 'react-i18next'
+
 interface NavList {
   title: string
   value: string
 }
-const navList: NavList[] = [
-  {
-    title: 'All',
-    value: 'all',
-  },
-  {
-    title: 'Follow',
-    value: 'follow',
-  },
-]
 
 export const NotificationPage = () => {
+  const { t } = useTranslation()
+
+  const navList: NavList[] = [
+    {
+      title: t('all'),
+      value: 'all',
+    },
+    {
+      title: t('following'),
+      value: 'follow',
+    },
+  ]
   return (
     <div className="w-full px-10 max-sm:px-0">
       <Tabs defaultValue="all">
-        <div className="fixed w-[71vw] z-10">
-          <div>
-            <TabsList className="flex justify-start bg-[#000]">
-              {navList.map((item, index) => (
-                <TabsTrigger
-                  className="flex-auto"
-                  key={index}
-                  value={item.value}
-                >
-                  {item.title}
-                </TabsTrigger>
-              ))}
-            </TabsList>
-          </div>
+        <div className="fixed z-10">
+          <TabsList className="flex justify-start bg-[#000]">
+            {navList.map((item, index) => (
+              <TabsTrigger className="min-w-24" key={index} value={item.value}>
+                {item.title}
+              </TabsTrigger>
+            ))}
+          </TabsList>
         </div>
         <TabsContent value="all">
           <NoticeCardList />
