be6559bb594a9605869960692236f8525f18084d
Birch <143273008+birchwork@users.noreply.github.com>
2024-12-21 09:34:27 +0800

    feact:update notice item

--- src/views/notification/index.tsx
+++ src/views/notification/index.tsx
@@ -4,6 +4,7 @@ import { PrimaryLayout } from '@/components/layouts/primary'
 import NoticeCardList from './components/notice-card-list'
 import { useTranslation } from 'react-i18next'
 import { NoticeAtion } from '@/api/user/types'
+
 interface NavList {
   title: string
   value: string
@@ -34,7 +35,6 @@ export const NotificationPage = () => {
     <div className="w-full px-10 max-sm:px-0">
       <Tabs defaultValue="all">
         <div className="fixed z-10">
-          {/* 消息导航 */}
           <TabsList className="flex justify-start bg-[#000]">
             {navList.map((item, index) => (
               <TabsTrigger className="min-w-28" key={index} value={item.value}>
@@ -43,7 +43,6 @@ export const NotificationPage = () => {
             ))}
           </TabsList>
         </div>
-        {/* 消息类别 */}
         {navList.map((item, index) => (
           <TabsContent key={index} value={item.value}>
             <NoticeCardList action={item.value} />
