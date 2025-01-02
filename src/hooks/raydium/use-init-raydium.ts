2202f3f95fecd7ab18d64d26af6da37b0f94c4d5
Leo <1909802953@qq.com>
2025-01-02 11:34:17 +0800

    fix: public key

--- src/hooks/raydium/use-init-raydium.ts
+++ src/hooks/raydium/use-init-raydium.ts
@@ -27,6 +27,7 @@ export const useInitRaydium = () => {
     const initRaydiumSDK = async () => {
         if (raydium) return
         if (!primaryWallet) return
+        if (primaryWallet.chain !== 'solana') return
         if (connection.rpcEndpoint === clusterApiUrl('mainnet-beta'))
             console.warn('using free rpc node might cause unexpected error, strongly suggest uses paid rpc node')
         console.log(`connect to rpc ${connection.rpcEndpoint} in ${programIds.network}`)
